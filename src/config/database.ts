99865e28f8e745622beaa02317a88862ea7653f9
karthik <krishnankarthik07@gmail.com>
2025-12-19 11:05:29 +0530

    feat:neon psql setup completed

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,13 +5,23 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+// export const AppDataSource = new DataSource({
+//   type: 'postgres',
+//   host: process.env.DB_HOST || 'localhost',
+//   port: parseInt(process.env.DB_PORT || '5433'),
+//   username: process.env.DB_USERNAME || 'postgres',
+//   password: process.env.DB_PASSWORD || '',
+//   database: process.env.DB_DATABASE || 'expense_tracker_test',
+// //   synchronize: true,
+//   entities: [User, Expense],
+// });
+
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5433'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'expense_tracker_test',
-//   synchronize: true,
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: [User, Expense],
+  synchronize: false,
 });
